c34b06c7903d5f3e6f6c27c83dd718095267afe6
Adrian Ibañez <hello@adrianibanez.info>
2025-02-23 16:05:43 +0100

    add rustler_precompiled dependency

--- mix.exs
+++ mix.exs
@@ -92,7 +92,7 @@ defmodule Todo.MixProject do
       {:gettext, "~> 0.23"},
       {:plug_cowboy, "~> 2.6"},
       {:jason, "~> 1.4"},
-      
+
       {:rustler_btleplug, "~> 0.0.6-alpha"},
       #{:rustler_btleplug,
       #      path: "/Users/adrianibanez/Documents/projects/2024_sensor-platform/checkouts/rustler_btleplug",
