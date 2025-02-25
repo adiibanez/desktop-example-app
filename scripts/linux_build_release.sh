ff53e7b23dda2fa6c222384850f26b0da4d0678a
Adrian Ibañez <hello@adrianibanez.info>
2025-02-26 00:24:55 +0100

    fixing linux containers

--- scripts/linux_build_release.sh
+++ scripts/linux_build_release.sh
@@ -2,27 +2,26 @@ cat $HOME/.bashrc
 ls -lah /usr/local/bin/asdf
 cat .tool-versions
 asdf plugin list
-asdf plugin add elixir
-asdf plugin add erlang
-asdf plugin add nodejs
+# asdf plugin add elixir
+# asdf plugin add erlang
+# asdf plugin add nodejs
 
 asdf current
 
-echo 'Running Script';
-# Compile and lint
-asdf install
-mix local.hex --force;
-mix local.rebar --force;
-mix deps.get
-mix deps.compile
-cd assets
-npm install
-# Build Release
-export LD_LIBRARY_PATH=/root/projects/wxWidgets/lib/;
-MIX_ENV=prod mix compile;
-# Build installer
-cd ..
-mix deps.update desktop
-mix deps.update desktop_deployment
-mix assets.deploy
-mix desktop.installer
+# echo 'Running Script';
+# # Compile and lint
+# mix local.hex --force;
+# mix local.rebar --force;
+# mix deps.get
+# mix deps.compile
+# cd assets
+# npm install
+# # Build Release
+# export LD_LIBRARY_PATH=/root/projects/wxWidgets/lib/;
+# MIX_ENV=prod mix compile;
+# # Build installer
+# cd ..
+# mix deps.update desktop
+# mix deps.update desktop_deployment
+# mix assets.deploy
+# mix desktop.installer
