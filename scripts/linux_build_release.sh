93e628c6d109d316dc2dd340e83eadb785dd4777
Adrian Ibañez <hello@adrianibanez.info>
2025-02-26 13:44:26 +0100

    only debug asdf installation for linux instead of building release

--- scripts/linux_build_release.sh
+++ scripts/linux_build_release.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # cat $HOME/.bashrc
 # ls -lah /usr/local/bin/asdf
 . ${ASDF_DIR}/asdf.sh
@@ -10,22 +11,24 @@ asdf reshim elixir
 asdf reshim erlang
 asdf reshim nodejs
 
-asdf install
+cat "*.log"
 
-echo 'Running Script';
-# Compile and lint
-mix local.hex --force;
-mix local.rebar --force;
-mix deps.get
-mix deps.compile
-cd assets
-npm install
-# Build Release
-export LD_LIBRARY_PATH=/root/projects/wxWidgets/lib/;
-MIX_ENV=prod mix compile;
-# Build installer
-cd ..
-mix deps.update desktop
-mix deps.update desktop_deployment
-mix assets.deploy
-mix desktop.installer
+# asdf install
+
+# echo 'Running Script';
+# # Compile and lint
+# mix local.hex --force;
+# mix local.rebar --force;
+# mix deps.get
+# mix deps.compile
+# cd assets
+# npm install
+# # Build Release
+# export LD_LIBRARY_PATH=/root/projects/wxWidgets/lib/;
+# MIX_ENV=prod mix compile;
+# # Build installer
+# cd ..
+# mix deps.update desktop
+# mix deps.update desktop_deployment
+# mix assets.deploy
+# mix desktop.installer
